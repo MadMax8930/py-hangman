0e4f7989ad12dd19d9d7eec17994172d3b2d5ebe
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-03-23 19:42:21 +0100

    global vars

---
+++ game.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+windowHeight = 480
+windowWidth = 700
+win=pygame.display.set_mode((windowWidth,windowHeight))
+
+#Global variables
+
+BLACK = (0,0, 0)
+WHITE = (255,255,255)
+RED = (255,0, 0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+LIGHT_BLUE = (102,255,255)
+
+btn_font = pygame.font.SysFont("arial", 20)
+guess_font = pygame.font.SysFont("monospace", 24)
+lost_font = pygame.font.SysFont('arial', 45)
+word = ''
+buttons = []
+guessed = []
